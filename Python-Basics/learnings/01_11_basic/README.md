e3dfca24bd507eae83738cf2cb789a7b3e0839e7
VishnuVishwas <gvishnuvishwas60@gmail.com>
2024-01-01 15:19:13 +0530

    Data types

--- Python-Basics/learnings/01_11_basic/README.md
+++ Python-Basics/learnings/01_11_basic/README.md
@@ -8,3 +8,30 @@
   - where & how variables can be used
 
 - rps game
+
+- main.py
+
+  - Data types
+
+    - Numeric Data types
+
+      - int
+      - float
+      - complex
+
+    - Sequencial Data types
+
+      - string
+
+        - Create String
+        - Multiline string
+        - Accessing characters in string
+        - string slicing
+        - Reversing a python string
+        - Deleting / Updating from a string
+
+      - Lists
+        -
+        -
+        -
+        -
--- Python-Basics/learnings/01_11_basic/main.py
+++ Python-Basics/learnings/01_11_basic/main.py
@@ -1,65 +1,161 @@
-# data types in python 
+# Data Types in Python
+# Numeric, Sequencial
 
-# int 
-number = 45
-print("Type of :", number, "is : ", type(number))
+# Numeric Data Types
+# int, float and Complex numbers
 
-# float
-number = 45.6
-print("Type of :", number, "is : ", type(number))
+# int
+a = 5
+print(type(a))
 
-# string 
-name = "Itachi"
-print("Type of :", name, "is : ", type(name))
+# float 
+b = 5.0
+print(type(b))
 
-# list    
-fruits = ['Mango', 'Apple']
-print("Type of :", fruits, "is : ", type(fruits))
+# Complex Numbers
+c = 2 + 4j
+print(type(c))
 
-# Boolean
-flag = True
-print(flag == False)    #checks if flag is eq to false
+# Sequence Data types in python
+# string, List, Tuple
 
-# Tuple 
-constants = (3.14, 9.8)
-print("Type of :", constants[0], "is : ", type(constants))
+# string - Immutable
+# create string - single line
+string1 = 'Welcome'
+print(string1)
 
-# set
-set = {'vishnu', 20, 'Male'}
-print("Type of :", set, "is : ", type(set))
+# multiline string
+string2 = '''
+Welcome
+Happy
+learning  '''
+print(string2)
 
-phone_book = {
-    1 : "Nagato",
-    2 : "Saske",
-    3 : "Madara"
-}
-print(type(phone_book))
+# Accessing characters in string
+string1 = "Vishnu Vishwas"
+print(string1[0])
+print(string1[-1])
 
-# variables are used to store data 
+# string slicing
+print(len(string1))
+print(string1[3: 9])   # last index not considered
+print(string1[3: -2])
 
-number = 435
-print(number)
-print(type(number))  # type() - data type of varible
+# Reversing a python string
+# Method - 1
+print(string1[::-1])
+# Method - 2
+string1 = "".join(reversed(string1))
+print(string1)
 
-name = "Vishwas"
-print(name)
-print(type(name))    # the data type will be string
+# Deleting / Updating from a string
 
-# concatenation of variable 
-print("Vishnu " + name)
+# 1. Convert string1-> List1 -> string2
+string1 = "Hello, I'm Vishnu"
+list1 = list(string1)
+list1[2] = 'P'
+string2 = "".join(list1)
+print(string2)
 
-# multiple assignment
-a = b = c = 45
-print(str(a) + " " + str(b) + " " + str(c))
+# 2. Slicing
+string3 = string1[0:2] + 'p' + string1[3:]
+print(string3)
 
-# multiple assgnment for different var in 1line
-a, b, c = 34, 44, 55
-print(str(a) + " " + str(b) + " " + str(c))
+# deleting a char & string
+string1 = "Vishnu Vishwas"
+# del string1[2] 
+# print(string1) 
 
-#concatenation of 2 variables
-fist_name = "Itachi"
-last_name = "Uciha"
+# using slicing 
+string2 = string1[0:2] + string1[3:]
+print(string2)
 
-print("Full name : " + fist_name + " " + last_name)
+del string1
+# print(string1)
 
+# Lists
+# lists are mutable
+
+# Create List
+list = ['Naruto', 5, 'Saitama', 6, 'Gojo']
+print(list)
+
+# Acessing 
+print(list[0:2])
+
+# Nested List
+list = [['Naruto', 5, 'Saitama', 6, 'Gojo'], ['Hokage', 'SuperHuman', 'Sensei']]
+print(list[1][2])
+
+# size of list
+print(len(list))
+
+# String to list
+# Split the words where there is space
+
+# Method 1 
+# string = input("Enter a string : ")
+# list = string.split()
+# print(list)
+# Enter a string : vishnu is a good boy
+# ['vishnu', 'is', 'a', 'good', 'boy']
+
+
+# Adding elements to list
+
+# 1. append()
+list = ['Naruto', 5, 'Saitama', 6, 'Gojo']
+list.append(100)
+print(list)
+
+# adding elements using a loop 
+for i in range(10, 40, 7):
+    list.append(i)
+print(list)
+
+# adding elements using a Tuple
+list.append((50, 232))
+print(list)
+
+# appending a list
+list1 = ("Zenin", "Gojo", "Uzumaki")
+list.append(list1)
+print(list)
+
+# 2. insert(position, value)
+list = ['1', 5, '2', 6, '3']
+list.insert(3, "Anya")
+print(list)
+
+# 3. extend() - To add multiple ele at the same time at the end of the list.
+list = ['1', 5, '2', 6, '3']
+list.extend(["krishna", "Radha"])
+print(list)
+
+# Reverse a list -> reverse()
+list = ['1', 5, '2', 6, '3']
+list.reverse()
+print("After reversing : ", list)
+
+# Removing elements from the list
+# 1. remove()
+list = [1, 2, 3, 4, 5, 6,7 , 8, 9, 10, 11, 12]
+print(list)
+list.remove(5)
+list.remove(6)
+print(list)
+
+# using iterator -  remove multiple elements
+for i in range(1, 5):
+    list.remove(i)
+print(list)
+
+# 2. using pop()
+print(list)
+print(list.pop())
+print(list)
+
+# removing element from specific location pop(location)
+list.pop(3)
+print(list)
 
