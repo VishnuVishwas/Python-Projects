e797d0b7682e162efcd0ebea779026a369626fe5
VishnuVishwas <gvishnuvishwas60@gmail.com>
2024-01-04 21:32:59 +0530

    f-strings

---
+++ Python-Basics/learnings/others/f_stings/main.py
@@ -0,0 +1,9 @@
+# f-strings
+# formatted string literals
+
+name = "Itachi"
+print(f"The protagonist of our story is {name} 😎")
+
+import datetime
+dob = datetime.datetime.today()
+print(f"{dob: %B %d %y}")
