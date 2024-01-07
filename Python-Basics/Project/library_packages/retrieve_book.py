37c3c9a823b4dc3b7aaef315efad538560192ba7
VishnuVishwas <gvishnuvishwas60@gmail.com>
2024-01-07 13:58:08 +0530

    Personal Library Management System

---
+++ Python-Basics/Project/library_packages/retrieve_book.py
@@ -0,0 +1,10 @@
+def retrieve_book(file_name):
+    try:
+        with open(file_name, 'r') as f:
+            for book in f:
+                print(book)
+    except FileNotFoundError:
+        print(f"The file {file_name} was not found")
+
+if __name__ == "__main__":
+    pass
