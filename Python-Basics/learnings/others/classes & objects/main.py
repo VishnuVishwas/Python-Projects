ffab04748b841c0c047ed4b041be3030a8a786f9
VishnuVishwas <gvishnuvishwas60@gmail.com>
2024-01-09 07:49:32 +0530

    class & objects

--- Python-Basics/learnings/others/classes & objects/main.py
+++ Python-Basics/learnings/others/classes & objects/main.py
@@ -1,23 +1,52 @@
-class Dog:
+# Bank System using oops 
 
-    # # Attributes
-    # attr1 = "mammal"
-    # attr2 = "dog"
+# Parent class = user()
+    # Holds the details of user
+    # has function to show user deatils 
+# Child class = bank()
+    # Stores details about account balance
+    # Stores details about amount
+    # Allows for deposite, withdraw, view_balance
 
-    # this class will be automatically called
-    def __init__(self, breed, age):
-        self.breed = breed
+# parent class
+class User():
+    def __init__(self, name, age, gender):
+        self.name = name
         self.age = age
+        self.gender = gender
 
-    def __str__(self) -> str:
-        print("This will print the content when a print is called.")
+    def show_details(self):
+        print("Personal details")
+        print("")
+        print("Name : ", self.name)
+        print("age : ", self.age)
+        print("gender : ", self.gender)
 
-    def fun(self ,name):
-        print("I'm a" , self.age)
-        print("I'm ", self.age)
-        print("My name is : ", name)
+# child class 
+class Bank(User):
+    def __init__(self, name, age, gender) :
+        super().__init__(name, age, gender)
+        self.balance = 0
 
-Rodger = Dog("Rodger", 8)
-Pug = Dog("Pug", 5)
-Rodger.fun("Tommy")
-Pug.fun("Blacky")
+    def deposit(self, amount):
+        self.amount = amount
+        self.balance += self.amount
+        print("Amount credited successfully.\nBalance: Rs. ", self.balance)
+
+    def withdraw(self, amount):
+        self.amount = amount
+        if self.amount > self.balance:
+            print("Insufficient balance.")
+        else:
+            self.balance -= self.amount
+            print("Balance : Rs. ", self.balance)
+
+    def view_balance(self):
+        self.show_details()
+        print("Account balance Rs. ", self.balance)
+
+if __name__ == "__main__":
+    vishnu = Bank('Vishnu', 20, "male")
+    vishnu.deposit(1000)
+    vishnu.withdraw(500)
+    vishnu.view_balance()
