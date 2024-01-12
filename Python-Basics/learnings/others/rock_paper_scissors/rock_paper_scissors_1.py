c76ae3f717ac11cd731f05fdca564aafac8b8078
VishnuVishwas <gvishnuvishwas60@gmail.com>
2024-01-12 11:51:13 +0530

    Rock, paper & scissors game version 1

--- Python-Basics/learnings/others/rock_paper_scissors/rock_paper_scissors_1.py
+++ Python-Basics/learnings/others/rock_paper_scissors/rock_paper_scissors_1.py
@@ -14,18 +14,18 @@ player_choice = int(input("Enter your choice : \n1.Rock\n2. Paper\n3. Scissors :
 if(player_choice < 1 | player_choice > 3):
     sys.exit("Your choice should be either 1, 2 or 3")
 
-computerChoice = int(random.choice("123"))
+computer_choice = int(random.choice("123"))
 
 print("Your choice : ", str(RockPaperScissors(player_choice).name))
-print("Computer choice : " + str(RockPaperScissors(computerChoice).name)) 
+print("Computer choice : " + str(RockPaperScissors(computer_choice).name)) 
 
-if player_choice == 1 and computerChoice == 3 :
+if player_choice == 1 and computer_choice == 3 :
     print("🏆 You won")
-elif player_choice == 2 and computerChoice == 1:
+elif player_choice == 2 and computer_choice == 1:
     print("🏆 You won")
-elif player_choice == 3 and computerChoice == 2:
+elif player_choice == 3 and computer_choice == 2:
     print("🏆 You won")
-elif player_choice == computerChoice:
+elif player_choice == computer_choice:
     print("🤝 Tie")
 else:
     print("💻 computer wins")
