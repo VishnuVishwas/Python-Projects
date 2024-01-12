86268507aa7ea666483ef77e3014861f1cadd1a8
VishnuVishwas <gvishnuvishwas60@gmail.com>
2024-01-12 11:32:51 +0530

    lamda

--- Python-Basics/learnings/others/lamda function/main.py
+++ Python-Basics/learnings/others/lamda function/main.py
@@ -10,7 +10,7 @@ format_numeric = lambda num: f"{num:e}" if isinstance(num, int) else f"{num:,.2f
 print("Int formmating: ", format_numeric(1000000000))
 print("Float formatting: ", format_numeric(999999.234234324324324234))
 
-#   
+# compare traditional function with lambda function
 def cube(y):
     return y*y*y
 print(cube(2))
