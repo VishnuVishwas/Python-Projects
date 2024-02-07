1f66be5b4e28c3060783e2e1fcce4eab9f56e8a7
G Vishnu Vishwas <gvishnuvishwas60@gmail.com>
2024-02-07 17:28:25 +0530

    Dog API

--- Rest-API/assignments/02_21_basics_of_web_APIs/main.py
+++ Rest-API/assignments/02_21_basics_of_web_APIs/main.py
@@ -1,20 +1,18 @@
-# Random dog image URL generator
-
 import requests
 import random
 
 def get_random_dog_image():
     # Fetch the list of all dog breeds
     url = 'https://dog.ceo/api/breeds/image/random'
-    responce = requests.get(url)
+    response = requests.get(url)
 
-    if responce.status_code == 200:
-        data = responce.json()
+    if response.status_code == 200:
+        data = response.json()
         # get a random dog image url
         image_url = data['message']
         print(f"Dog image URL: {image_url}")
     else:
-        print(f"Failed to fetch dog image URL. Status code {responce.status_code}")
+        print(f"Failed to fetch dog image URL. Status code {response.status_code}")
 
 if __name__ == '__main__':
     get_random_dog_image()
