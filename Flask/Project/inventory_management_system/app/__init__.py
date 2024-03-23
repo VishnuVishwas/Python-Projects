12ad65a8c1a05f82e195650da03b927db78677ed
G Vishnu Vishwas <gvishnuvishwas60@gmail.com>
2024-03-23 20:56:52 +0530

    Inventory management system

---
+++ Flask/Project/inventory_management_system/app/__init__.py
@@ -0,0 +1,44 @@
+# app/__init__.py
+
+from flask import Flask, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+# flask app instance
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'c4e53ee2876e8fed39373a33a80bd15a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+bcrypt = Bcrypt(app)
+
+# initialize flask-login
+login_manager = LoginManager(app)
+
+# Unauthorized handler 
+@login_manager.unauthorized_handler
+def unauthorized():
+    flash('You need to log in first.', 'danger')
+    return redirect('/login-user')
+
+@login_manager.user_loader
+def load_user(user_id):
+    from app.users.models import SignupUser
+    from app.admin.models import Admin
+
+    user = SignupUser.query.get(int(user_id))
+    if user:
+        return user
+    
+    admin = Admin.query.get(int(user_id))
+    if admin:
+        return admin
+
+# db instance
+db = SQLAlchemy(app)
+
+from app.admin import routes
+from app.users import routes
+from app.products.models import Products
+from app.products import routes
+from app.users.models import SignupUser
+from app.admin.models import Admin
---
+++ Flask/Project/inventory_management_system/app/products/routes.py
@@ -0,0 +1,89 @@
+# app/products/routes.py
+
+from app import app, db
+from flask import Flask, json, request, jsonify, render_template, redirect
+from app.products.models import Products
+from app.users.models import SignupUser
+from flask_login import login_required, current_user
+
+
+# get product page
+@app.route('/products-page', methods=['GET'])
+@login_required
+def render_product_page():
+    products = Products.query.all()
+    return render_template('products/products.html', products=products)
+
+# create a product
+@app.route('/products', methods=['POST'])
+@login_required
+def create_product():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        price = request.form.get('price')
+        category = request.form.get('category')
+
+        user = SignupUser.query.filter_by(id=current_user.id).first()
+        if user:
+            user_name = current_user.username
+        else:
+            user_name = "Unknown"
+
+        new_product = Products(name=name, price=price, category=category, user_id=current_user.id, user_name=user_name)
+
+        db.session.add(new_product)
+        db.session.commit()
+
+        # Redirect to the products page after adding the product
+        return redirect('/products-page')
+
+    # If the request method is not POST, render the products page
+    return render_template('products/products.html')
+    
+# update product
+@app.route('/products/<int:id>', methods=['GET', 'POST'])
+@login_required
+def update_product(id):
+    product = Products.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        name = request.form.get('name')
+        price = request.form.get('price')
+        category = request.form.get('category')
+
+        # Check if any of the fields are empty, if so, retain the original values
+        if not name:
+            name = product.name
+        if not price:
+            price = product.price
+        if not category:
+            category = product.category
+
+        product.name = name
+        product.price = price
+        product.category = category
+
+        db.session.commit()
+
+        # Redirect to the products page after updating the product
+        return redirect('/products-page')
+
+    # Render the update form with pre-filled values
+    return render_template('products/update.html', product=product)
+
+
+# delete product
+@app.route('/products/delete/<int:id>', methods=['POST'])
+@login_required
+def delete_product(id):
+
+    if request.method == 'POST':
+        if request.form.get('_method') == 'DELETE':
+            product = Products.query.get_or_404(id)
+            if product:
+                db.session.delete(product)
+                db.session.commit()
+                return redirect('/products-page')
+    
+    return jsonify({'error': 'Method Not Allowed'}), 405
+
